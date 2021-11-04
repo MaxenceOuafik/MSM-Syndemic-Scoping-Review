7770d8fc899285db397b7bd60dbc02acabf78ff7
Asrahal <maxence.ouafik@gmail.com>
2021-11-04 21:23:55 +0100

    Cross-reference issue fixed

--- Scripts/supplementary_tables/PRISMA-ScR.R
+++ Scripts/supplementary_tables/PRISMA-ScR.R
@@ -2,7 +2,7 @@ library(readxl)
 source("./scripts/supplementary_tables/general.R")
 
 .PRISMA_Scr_Checklist <- read_excel("Files/PRISMA_Scr_Checklist.xlsx")
-.PRISMA_FT <- flextable(.PRISMA_Scr_Checklist, cwidth = c(2, 0.5, 4.5, 0.9)) %>%
+.PRISMA_ft <- flextable(.PRISMA_Scr_Checklist, cwidth = c(2, 0.5, 4.5, 0.9)) %>%
   border_inner(part="body", border = .borders) %>%
   border_outer(part="body", border = .big.border) %>%
   border_outer(part="header", border = .outer.border) %>%
@@ -10,6 +10,6 @@ source("./scripts/supplementary_tables/general.R")
   bold(part="header") %>%
   color(part="header", color="white") 
 
-.PRISMA_ft <- set_caption(.CSA_ft, 
+.PRISMA_ft <- set_caption(.PRISMA_ft, 
                           "Preferred Reporting Items for Systematic reviews and Meta-Analyses extension for Scoping Reviews (PRISMA-ScR) Checklist",
                        autonum = .supp_autonum)
